88dd48f107d7efd212c72183b53f20f893dea0f1
felipe-barreto-dev <fbarreto.dev@gmail.com>
2023-06-25 15:01:52 -0300

    chore: configure eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const getNews = async () => {
+    return await axios.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=7be58569255242d4871ac1375f33027a')
+  }
+  const { data, isError, isLoading } = useQuery(["news"], () => getNews())
+
+  console.log(data, isLoading, isError)
   return (
     <>
     </>
